d43d3bb7e6c0d5bb3dc676a4814fbd89ec76016f
eonaranas <aaronaranas@gmail.com>
2022-03-24 12:01:02 +0800

    + make 2 types of jump trigger high jump and low jump to set which animation to play

--- My project/Assets/Scripts/AnimationTrigger/JumpTrigger.cs
+++ My project/Assets/Scripts/AnimationTrigger/JumpTrigger.cs
@@ -3,8 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpTrigger : AnimationTrigger {
+
+	public float jumpForce = 550f;
+	public enum JUMP_ANIMATION { LOW_JUMP = 0, HIGH_JUMP }
+	public JUMP_ANIMATION jumpAnimationToPlay;
 	public override void DoInputForPlayer(PlayerInput p_input) {
 		p_input.pressJumpFromTrigger = true;
-		p_input.animator.PlayLowJump();
+		p_input.isPlayer = false;
+		PlayerController pc = p_input.GetComponent<PlayerController>();
+		pc.enabled = false;
+		Rigidbody rb = p_input.GetComponent<Rigidbody>();
+		rb.Sleep();
+		rb.velocity = Vector3.zero;
+		rb.isKinematic = false;
+		rb.useGravity = true;
+		if (jumpAnimationToPlay == JUMP_ANIMATION.HIGH_JUMP) {
+			p_input.animator.PlayHighJump();
+		} else {
+			p_input.animator.PlayLowJump();
+		}
+		
+
+		rb.AddForce((p_input.transform.forward + p_input.transform.up) * jumpForce);
+		StartCoroutine(CheckIfGrounded(pc, p_input, rb));
+	}
+
+	IEnumerator CheckIfGrounded(PlayerController pc, PlayerInput pi, Rigidbody rb) {
+		GroundDetector gd = GameObject.FindObjectOfType<GroundDetector>();
+		yield return new WaitForSeconds(0.5f);
+		while (!gd.isGrounded) {
+			Debug.LogError(gd.isGrounded);
+			yield return 0;
+		}
+		Debug.LogError(gd.isGrounded);
+		pc.enabled = true;
+		pi.isPlayer = true;
+		rb.useGravity = false;
 	}
 }
